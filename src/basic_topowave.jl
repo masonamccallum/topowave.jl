e877c24af64dfd4aa5b21230902531c3c9200d74
Mason McCallum <masonmccallum@Masons-MacBook-Air.local>
2022-08-29 15:33:00 -0500

    RK4 implement

--- src/basic_topowave.jl
+++ src/basic_topowave.jl
@@ -2,7 +2,6 @@ using LinearAlgebra, Plots
 using ProgressBars
 using UnPack
 using StartUpDG
-using OrdinaryDiffEq
 
 #PDE: ħ∂ₜu = -σ₁uₓ-σ₂uᵥ-imσ₃u
 #NOTE:v=y  VS code won't let me type u\_y
@@ -23,6 +22,7 @@ initial(x,y) = reshape([
     ℯ^(-(1/200)*im*((x-50)^2+(y-50)^2))
     ℯ^(-(1/200)*im*((x-50)^2+(y-50)^2))
 ],1,2);
+
 
 begin
 import_options = MeshImportOptions(true,true)
@@ -53,86 +53,104 @@ scatter!(x[Fmask[:],:],y[Fmask[:],:],markercolor="white",markersize=3,size=(1000
 #scatter!(xf,yf,markersize=2)
 end
 
-begin
-@unpack x,y = md;
-u_temp = @. initial(x, y);
-u = zeros(Complex,size(u_temp,1),size(u_temp,2),2)
-for i in 1:size(u_temp,1)
-    for j in 1:size(u_temp,2)
-        u[i,j,1] = u_temp[i,j][1]
-        u[i,j,2] = u_temp[i,j][2]
-    end
-end
-Plots.scatter(x, y, real.(u[:,:,1]), leg=false, markersize=0.5)
-end
+
 
 #   Jᵏ∂ₜu = -Dᵣσ₁u -Dₛσ₂u - i*m*σ₃u - M⁻¹∫̂n⋅(σ₁(u*-uᵏ)+σ₂(u*-uᵏ))φₙdΓ
 #         TODO: Equation above missing geometric factors
-#         TODO: understand mesh variety
 #         TODO: add boundary conditions
-#         TODO: add flux term
 #         TODO: intialize wave based on Dispersion Relation
 
-    σ₁ = [0 1; 1 0];
-function rhs!(du, u, parameters, t) #TODO: combine terms and optimize memory usage 
+function rhs!(du, u, parameters) #TODO: combine terms and optimize memory usage 
     @unpack rd, md = parameters
     @unpack Vf, Fmask, Dr, Ds, LIFT = rd
     @unpack rxJ, sxJ, ryJ, syJ, J, nxJ, nyJ, sJ = md
     @unpack uf, uP, flux_u, ur, us, dudx, lifted_flux = parameters
+    @unpack dr_sig1_u, ds_sig2_u = parameters
 
     σ₁ = [0 1; 1 0];
     σ₂ = [0 -im; im 0];
     σ₃ = [1 0; 0 -1];
 
-    #Vf: face quad interp map
+    #Vf: face interp map 12 per element
     mul!(uf, complex.(Vf), u[:,:,1])
     @. uP = uf[md.mapP]
     @. flux_u = 0.5 * (uP - uf) * nxJ
-    lifted_flux = LIFT * flux_u
+    lifted_flux[:,:,1] = LIFT * flux_u
 
     mul!(uf, complex.(Vf), u[:,:,2])
     @. uP = uf[md.mapP]
     @. flux_u = 0.5 * (uP - uf) * nxJ
-    lifted_flux = LIFT * flux_u
-    println(size(lifted_flux))
+    lifted_flux[:,:,2] = LIFT * flux_u
 
-    # u [10*1160*2]
     uflat = transpose(reshape(u,size(u,1)*size(u,2),2)) # [2*11600]
     sig1_u = reshape(σ₁ * uflat,size(u,1),size(u,2),2)  # [10*1160*2]
     sig2_u = reshape(σ₂ * uflat,size(u,1),size(u,2),2)
     sig3_u = reshape(σ₃ * uflat,size(u,1),size(u,2),2)
 
-    dr_sig1_u1 = -Dr*sig1_u[:,:,1]
-    dr_sig1_u2 = -Dr*sig1_u[:,:,2]
+    dr_sig1_u[:,:,1] = -Dr*sig1_u[:,:,1]
+    dr_sig1_u[:,:,2] = -Dr*sig1_u[:,:,2]
 
-    ds_sig2_u1 = -Ds*sig2_u[:,:,1]
-    ds_sig2_u2 = -Ds*sig2_u[:,:,2]
+    ds_sig2_u[:,:,1] = -Ds*sig2_u[:,:,1]
+    ds_sig2_u[:,:,2] = -Ds*sig2_u[:,:,2]
 
-    #=
-    mul!(sig1_u,σ₁,uflat)
-    sig1_u = transpose(sig1_u)
-    sig1_u = reshape(sig1_u,size(u,1),size(u,2),size(u,3))
-    dr_sig1_u = reshape([Dr*sig1_u[:,:,1] Dr*sig1_u[:,:,2]],size(u,1),size(u,2),2)
-
-    dr_sig2_u = similar(u)
-    mul!(sig2_u,σ₂,uflat)
-    sig2_u = transpose(sig2_u)
-    sig2_u = reshape(sig2_u,size(u,1),size(u,2),size(u,3))
-    dr_sig2_u = reshape([Dr*sig2_u[:,:,1] Dr*sig2_u[:,:,2]],size(u,1),size(u,2),2)
-
-    mul!(sig3_u,σ₃ ,uflat)
-    sig3_u = transpose(sig3_u)
-    sig3_u = reshape(sig3_u,size(u,1),size(u,2),size(u,3))
-    return -dr_sig1_u-dr_sig2_u-im*0.2*sig3_u
-    =#
+    @. dudx[:,:,1] = dr_sig1_u[:,:,1] + ds_sig2_u[:,:,1] - im*0.2*sig3_u[:,:,1] 
+    @. dudx[:,:,1] = rxJ * dr_sig1_u[:,:,1] + sxJ * ds_sig2_u[:,:,1] + im*0.2*sig3_u[:,:,1] 
+    @. dudx[:,:,2] = rxJ * ds_sig2_u[:,:,2] + sxJ * ds_sig2_u[:,:,2] + im*0.2*sig3_u[:,:,2] 
+    @. du = dudx + lifted_flux
+    du ./= -J
+    return du
 end
+# 610517
+# Test my RHS! using function I know derivative of
+# m2: amd queue gpu node
 
 begin
 tspan = (0.0, 1.0)
+@unpack x,y = md;
+    u_temp = @. initial(x, y);
+    u = zeros(Complex,size(u_temp,1),size(u_temp,2),2)
+    for i in 1:size(u_temp,1)
+        for j in 1:size(u_temp,2)
+            u[i,j,1] = u_temp[i,j][1]
+            u[i,j,2] = u_temp[i,j][2]
+        end
+    end
+
+
 parameters = (; rd, md, uf=similar(md.xf,Complex), uP=similar(md.xf,Complex), flux_u=similar(md.xf,Complex),
-    ur=similar(zeros(size(md.x,1),size(md.x,2),2),Complex), us=similar(md.x,Complex), dudx=similar(md.x,Complex), lifted_flux=similar(md.x,Complex))
-rhs!(0.1,u,parameters,1)
+    ur=similar(zeros(size(md.x,1),size(md.x,2),2),Complex),ds_sig2_u=similar(u),dr_sig1_u=similar(u), us=similar(md.x,Complex), dudx=similar(u), lifted_flux=similar(u))
+du = zeros(Complex,size(u))
+#prob = ODEProblem(rhs!, u, tspan, parameters)
 end
 
-#prob = ODEProblem(rhs!, u, tspan, parameters)
-#sol = solve(prob, Tsit5(), reltol=1e-7, abstol=1e-7)
+function topo2d(u,parameters,FinalTime)
+    rk4a = [            0.0,
+            -567301805773.0/1357537059087.0,
+            -2404267990393.0/2016746695238.0,
+            -3550918686646.0/2091501179385.0,
+            -1275806237668.0/842570457699.0];
+    rk4b = [ 1432997174477.0/9575080441755.0,
+            5161836677717.0/13612068292357.0,
+            1720146321549.0/2090206949498.0,
+            3134564353537.0/4481467310338.0,
+            2277821191437.0/14882151754819.0];
+            
+    time=0
+    resU = zeros(Complex,size(u)) # Fix
+    xmin = 1.0 
+    CFL = 0.05;
+    dt=CFL*xmin;
+    Nsteps = ceil(FinalTime/dt);
+    dt = FinalTime/Nsteps
+    anim1 = @animate for tstep in ProgressBar(1:Nsteps)
+        for INTRK=1:5
+            rhsU = rhs!(du,u,parameters);
+            resU = rk4a[INTRK]*resU + dt*rhsU;
+            u = u+rk4b[INTRK]*resU;
+            Plots.scatter(x, y, real.(u[:,:,1]), leg=false, markersize=0.5)
+        end
+        time = time+dt;
+    end
+    gif(anim1, "./topo.gif", fps=60)
+    return u 
+end
